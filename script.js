f1a2ea69fecf59061da9863e7c3227441faa26e7
nespolo24 <nespolo@riseup.net>
2024-08-07 00:40:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const cursor = document.createElement('div');
-    cursor.className = 'custom-cursor';
-    cursor.textContent = '⚡';
-    document.body.appendChild(cursor);
-
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.left = e.clientX + 'px';
-        cursor.style.top = e.clientY + 'px';
-    });
-
     const widgets = document.querySelectorAll('.widget');
     widgets.forEach(widget => {
         widget.addEventListener('click', function() {
