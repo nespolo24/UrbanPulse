79065d2094eeb6d716ab37612283031c082c6b57
nespolo24 <nespolo@riseup.net>
2024-08-06 22:40:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', (e) => {
+    const cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
+
+function toggleActive(element) {
+    element.classList.toggle('active');
+}
