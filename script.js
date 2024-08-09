ee43beb5a7f9db6ec83223d6fe56c40757f7e607
nespolo24 <nespolo@riseup.net>
2024-08-09 12:42:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }, 500);
         });
     });
+
+    // Smooth scroll for internal links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
